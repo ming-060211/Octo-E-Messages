2e4587681b595834594ee09bfa9853e721d27d17
ming <ming.xuan.leong@wazp.io>
2024-03-06 11:22:42 +0000

    Updated for Gcloud Deploy

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -75,12 +75,14 @@
     </a>
     <ul class="hidden absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 lg:flex lg:mx-auto lg:flex lg:items-center lg:w-auto lg:space-x-6">
         <li><a class="text-sm text-gray-400 hover:text-gray-500" href="/">Home</a></li>
+        {#if data.session}
         <li class="text-gray-300">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" class="w-4 h-4 current-fill" viewBox="0 0 24 24">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
             </svg>
         </li>
-        <li><button class="text-sm text-blue-600 font-bold" on:click={() => getmodaladdnewuser()}>Add New User</button></li>
+            <li><button class="text-sm text-blue-600 font-bold" on:click={() => getmodaladdnewuser()}>Add New User</button></li>
+        {/if}
         <li class="text-gray-300">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" class="w-4 h-4 current-fill" viewBox="0 0 24 24">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
