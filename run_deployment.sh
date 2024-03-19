9ed5c22b785edd3f9e346852d50f83f819d57935
ming <ming.xuan.leong@wazp.io>
2024-03-19 11:04:55 +0000

    done remove

---
+++ run_deployment.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+source ./setup_build_env.sh
+
+gcloud builds submit --substitutions=_SUPABASE_URL_PARAM=$SUPABASE_URL_PARAM,_SUPABASE_ANON_KEY_PARAM=$SUPABASE_ANON_KEY_PARAM,_CRYPTER_KEY_PARAM=$CRYPTER_KEY_PARAM,_PUBLIC_SUPABASE_URL_PARAM=$PUBLIC_SUPABASE_URL_PARAM,_PUBLIC_SUPABASE_ANON_KEY_PARAM=$PUBLIC_SUPABASE_ANON_KEY_PARAM,_RESEND_KEY_PARAM=$RESEND_KEY_PARAM
+
+gcloud run deploy usermanagementapp --image gcr.io/hive-347910/usermanagementapp:latest --region=europe-west1 --allow-unauthenticated
