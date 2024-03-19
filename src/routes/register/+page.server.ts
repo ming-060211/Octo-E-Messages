75b6b9c087a78a20cb211c49735b9eb07727ef95
ming <ming.xuan.leong@wazp.io>
2024-03-19 11:01:40 +0000

    done make changes

--- src/routes/register/+page.server.ts
+++ src/routes/register/+page.server.ts
@@ -1,10 +1,6 @@
 import type {Actions} from "@sveltejs/kit";
 // import { supabase } from '../../hooks.server'
-<<<<<<< HEAD
 import {fail, json} from "@sveltejs/kit";
-=======
-import {fail} from "@sveltejs/kit";
->>>>>>> 5388004 (Initial commit)
 import {redirect, setFlash} from "sveltekit-flash-message/server";
 
 export const load = async (event) => {
@@ -21,15 +17,11 @@ export const actions : Actions  = {
         const password = formData.get('password') as string
         const confirm_password = formData.get('confirm_password') as string
         //if account had register is will have no error and no return bacuse it might cause data leak (supabase feature)
-<<<<<<< HEAD
         if (password !== confirm_password){
             setFlash({type: "error", message: "Invalid Input"}, cookies)
             return fail(400)
         }
         const { error } = await supabase.auth.signUp({
-=======
-            const { error } = await supabase.auth.signUp({
->>>>>>> 5388004 (Initial commit)
             email,
             password,
             options: {
@@ -38,16 +30,11 @@ export const actions : Actions  = {
         })
 
         if (error) {
-<<<<<<< HEAD
             // console.log(error.message)
             // return fail(500, { message: 'Server error. Try again later.', success: false, email })
             // return json("Please Try Again Later")
             setFlash({type: "error", message: error.message}, cookies)
             return fail(400)
-=======
-            console.log(error.message)
-            return fail(500, { message: 'Server error. Try again later.', success: false, email })
->>>>>>> 5388004 (Initial commit)
         }
 
         // console.log("the chcek email thingy")//it work
